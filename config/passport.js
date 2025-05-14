7665e930ac192355ae3debacdf6b57fafcddc12b
Manish Gupta <manishgupta724872@gmail.com>
2025-05-14 12:04:42 +0530

    api env fixed

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:4000/api/auth/google/callback",
+      callbackURL: `${process.env.NEXT_PUBLIC_APP_URL} /auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       const user = {
